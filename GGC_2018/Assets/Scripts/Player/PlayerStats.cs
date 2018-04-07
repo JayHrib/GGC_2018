f79eb8ad0902d709ba439422fc04c914d1ce6e9d
JayHrib <JayHrib@live.se>
2018-04-07 18:40:31 +0200

    First project upload
    Created Unity2D project
    Added PDollar add-on for gesture recognition
    Modified PDollar to suit our needs (WIP)
    IMPORTANT NOTE!: Templates for added spells can be found in the folder at the following adress:
    GitHub\"YOURFOLDER"\GGC_2018\Assets\PDollar\Resources\GestureTemplate

---
+++ GGC_2018/Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    public float healthPoints = 100f;
+    public bool playerOne = false;
+    public bool playerTwo = false;
+
+    private float currentHealth;
+   
+	// Use this for initialization
+	void Start () {
+        currentHealth = healthPoints;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
