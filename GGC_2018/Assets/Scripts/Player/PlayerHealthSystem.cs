7be78d98cbd27c4d70cc1e0840a6fda3abe68c12
JayHrib <JayHrib@live.se>
2018-04-09 16:07:28 +0200

    Update
    Player healthsystem added
    Tags (Enemy, Spell) added
    Rigidbodies and colliders added to enemy and player in scene

---
+++ GGC_2018/Assets/Scripts/Player/PlayerHealthSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerStats))]
+public class PlayerHealthSystem : MonoBehaviour {
+
+    private float currentHealth;
+    private float baseDamage = 20f;
+
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = GetComponent<PlayerStats>().maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            TakeDamage(baseDamage);
+        }
+    }
+
+    void TakeDamage(float damage)
+    {
+        currentHealth -= baseDamage;
+
+        if (baseDamage <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
