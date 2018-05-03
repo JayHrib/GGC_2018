6671a7e7d248a36045bca1fe1744c04d720ceefb
Jon <37902059+Yibeh@users.noreply.github.com>
2018-05-03 10:49:09 +0200

    lanes n stuff
    also some particle stuff i gues

---
+++ GGC_2018/Assets/Scripts/_Systems/Aimer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aimer : MonoBehaviour {
+
+    public KeyCode left;
+    public KeyCode right;
+
+    public int lane = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(left))
+        {
+            if(lane == 1)
+            {
+                lane = 6;
+            }
+            else
+            {
+                lane--;
+            }
+        }
+        if (Input.GetKeyDown(right))
+        {
+            if (lane == 6)
+            {
+                lane = 1;
+            }
+            else
+            {
+                lane++;
+            }
+        }
+        float poz = 1.5f;
+        switch (lane)
+        {
+            case 1:
+                poz = -7.5f;
+                break;
+            case 2:
+                poz = -4.5f;
+                break;
+            case 3:
+                poz = -1.5f;
+                break;
+            case 4:
+                poz = 1.5f;
+                break;
+            case 5:
+                poz = 4.5f;
+                break;
+            case 6:
+                poz = 7.5f;
+                break;
+            default:
+                break;
+        }
+        transform.position = new Vector3(poz,4,0);
+	}
+}
