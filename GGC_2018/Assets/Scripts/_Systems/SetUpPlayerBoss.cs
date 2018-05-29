b0b74c2086fd41dbb0c31c390b805cbb249fa334
JayHrib <JayHrib@live.se>
2018-05-29 20:32:22 +0200

    Update
    * Added button functionality to main menu
    * Player can now move in boss fight again
    + Player character now changes in boss scene depending on the chosen character

---
+++ GGC_2018/Assets/Scripts/_Systems/SetUpPlayerBoss.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetUpPlayerBoss : MonoBehaviour {
+
+    public GameObject player;
+
+    public RuntimeAnimatorController cont_bart;
+    public RuntimeAnimatorController cont_bokaj;
+
+    public Sprite spr_bart;
+    public Sprite spr_bokaj;
+
+    private string character;
+
+	// Use this for initialization
+	void Awake () {
+        character = PlayerPrefs.GetString("Character");
+        Setup(character);
+	}
+	
+    private void Setup(string character)
+    {
+        if (character == "Bartholomew")
+        {
+            player.GetComponent<SpriteRenderer>().sprite = spr_bart;
+            player.GetComponent<Animator>().runtimeAnimatorController = cont_bart;
+        }
+
+        else if (character == "Bokaj")
+        {
+            player.GetComponent<SpriteRenderer>().sprite = spr_bokaj;
+            player.GetComponent<Animator>().runtimeAnimatorController = cont_bokaj;
+        }
+    }
+}
